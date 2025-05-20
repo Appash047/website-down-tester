f66536d8f0a576c71bb5b4edcf9ffa5f1a236713
Appash047 <p.appash047@gmail.com>
2025-05-20 16:55:07 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default function App() {
       setIsSending(true);
       setSentRequests(0);
 
-      const response = await fetch('http://51.21.160.142:8080/api/send-requests', {
+      const response = await fetch('https://f976-13-48-42-168.ngrok-free.app/api/send-requests', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ url, count: countLimit }),
